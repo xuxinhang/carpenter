7a482d4cc455ea3ed5ee057dcf9e066219a438d8
xuxinhang <xuxinhang4567@126.com>
2022-12-25 21:24:41 +0800

    Fix: the loss of some plain text bytes in SNI Rewritter.

--- crate/transformer/sni.rs
+++ crate/transformer/sni.rs
@@ -1,17 +1,18 @@
 use std::io;
 use std::io::{Read, Write};
 use rustls::{ServerConnection, ClientConnection, ServerConfig, ClientConfig};
-// use super::buffer::StreamBuffer;
+use super::streambuffer::StreamBuffer;
 use super::certstorage::get_cert_data_by_hostname;
 use super::{TransformerResult, TransformerPortState, Transformer};
 use crate::common::Hostname;
 
+const SINGLE_BRUST_SIZE_LIMIT: usize = 512 * 1024; // = 512 KB
 
 pub struct SniRewriterTransformer {
     local_tls: ServerConnection,
     remote_tls: ClientConnection,
-    // transmit_text: StreamBuffer,
-    // receive_text: StreamBuffer,
+    transmit_text: StreamBuffer,
+    receive_text: StreamBuffer,
 }
 
 impl SniRewriterTransformer {
@@ -51,14 +52,18 @@ impl SniRewriterTransformer {
         }
         let remote_tls_conf = std::sync::Arc::new(remote_tls_conf);
 
+        let mut local_tls =  ServerConnection::new(local_tls_conf).unwrap();
+        local_tls.set_buffer_limit(Some(SINGLE_BRUST_SIZE_LIMIT * 2));
+        let mut remote_tls = ClientConnection::new(remote_tls_conf,
+            convert_hostname_to_rustls_server_name(new_sni.unwrap_or(raw_sni).clone()),
+        ).unwrap();
+        remote_tls.set_buffer_limit(Some(SINGLE_BRUST_SIZE_LIMIT * 2));
+
         Ok(Self {
-            local_tls: ServerConnection::new(local_tls_conf).unwrap(),
-            remote_tls: ClientConnection::new(
-                remote_tls_conf,
-                convert_hostname_to_rustls_server_name(new_sni.unwrap_or(raw_sni).clone()),
-            ).unwrap(),
-            // transmit_text: StreamBuffer::new(),
-            // receive_text: StreamBuffer::new(),
+            local_tls, 
+            remote_tls,
+            transmit_text: StreamBuffer::new(),
+            receive_text: StreamBuffer::new(),
         })
     }
 }
@@ -97,10 +102,16 @@ impl Transformer for SniRewriterTransformer {
         let plaintext_size = state.plaintext_bytes_to_read();
         let peer_closed = state.peer_has_closed();
 
+        // read out plain texts, write them into its pair tls or the buffer. 
         let mut text_buffer = Vec::new();
         text_buffer.resize(plaintext_size, 0);
-        let _text_read_size = self.local_tls.reader().read(&mut text_buffer).unwrap();
-        let _text_write_size = self.remote_tls.writer().write(&text_buffer).unwrap();
+        let text_read_size = self.local_tls.reader().read(&mut text_buffer).unwrap();
+        assert_eq!(text_read_size, plaintext_size); 
+        let text_write_size = self.remote_tls.writer().write(&text_buffer).unwrap();
+        if text_write_size < text_read_size {
+            self.transmit_text.write(&text_buffer[text_write_size..]).unwrap();
+        }
+
         if peer_closed {
             self.remote_tls.send_close_notify();
         }
@@ -109,7 +120,7 @@ impl Transformer for SniRewriterTransformer {
     }
 
     fn transmit_readable(&self) -> TransformerPortState {
-        if self.remote_tls.wants_write() {
+        if self.remote_tls.wants_write() || self.transmit_text.readable_size() > 0 {
             TransformerPortState::Open(-1)
         } else {
             TransformerPortState::Open(0)
@@ -118,7 +129,17 @@ impl Transformer for SniRewriterTransformer {
 
     fn transmit_read(&mut self, mut buf: &mut [u8]) -> TransformerResult {
         if !self.remote_tls.wants_write() {
-            return TransformerResult::Ok(0);
+            // try to feed some plain text
+            let plaintext_size = std::cmp::min(self.transmit_text.readable_size(), SINGLE_BRUST_SIZE_LIMIT);
+            let mut plaintext_buf = Vec::new();
+            plaintext_buf.resize(plaintext_size, 0);
+            self.transmit_text.read(&mut plaintext_buf).unwrap();
+            self.remote_tls.writer().write(&plaintext_buf).unwrap();
+
+            // then check again
+            if !self.remote_tls.wants_write() {
+                return TransformerResult::Ok(0);
+            }
         }
 
         let write_tls_result = self.remote_tls.write_tls(&mut buf);
@@ -160,10 +181,16 @@ impl Transformer for SniRewriterTransformer {
         let plaintext_size = state.plaintext_bytes_to_read();
         let peer_closed = state.peer_has_closed();
 
+        // read out plain texts, write them into its pair tls or the buffer. 
         let mut text_buffer = Vec::new();
         text_buffer.resize(plaintext_size, 0);
-        let _text_read_size = self.remote_tls.reader().read(&mut text_buffer).unwrap();
-        let _text_write_size = self.local_tls.writer().write(&text_buffer).unwrap();
+        let text_read_size = self.remote_tls.reader().read(&mut text_buffer).unwrap();
+        assert_eq!(text_read_size, plaintext_size); 
+        let text_write_size = self.local_tls.writer().write(&text_buffer).unwrap();
+        if text_write_size < text_read_size {
+            self.receive_text.write(&text_buffer[text_write_size..]).unwrap();
+        }
+
         if peer_closed {
             self.remote_tls.send_close_notify();
         }
@@ -172,7 +199,7 @@ impl Transformer for SniRewriterTransformer {
     }
 
     fn receive_readable(&self) -> TransformerPortState {
-        if self.local_tls.wants_write() {
+        if self.local_tls.wants_write() || self.receive_text.readable_size() > 0 {
             TransformerPortState::Open(-1)
         } else {
             TransformerPortState::Open(0)
@@ -181,7 +208,17 @@ impl Transformer for SniRewriterTransformer {
 
     fn receive_read(&mut self, mut buf: &mut [u8]) -> TransformerResult {
         if !self.local_tls.wants_write() {
-            return TransformerResult::Ok(0);
+            // try to feed some plain text
+            let plaintext_size = std::cmp::min(self.receive_text.readable_size(), SINGLE_BRUST_SIZE_LIMIT);
+            let mut plaintext_buf = Vec::new();
+            plaintext_buf.resize(plaintext_size, 0);
+            self.receive_text.read(&mut plaintext_buf).unwrap();
+            self.local_tls.writer().write(&plaintext_buf).unwrap();
+
+            // then check again
+            if !self.local_tls.wants_write() {
+                return TransformerResult::Ok(0);
+            }
         }
 
         let write_tls_result = self.local_tls.write_tls(&mut buf);
